7e6283cfe74d8336b3d3c4ee157a343ae78558d1
pdrobek <pawel.drobek@gmail.com>
2014-08-23 18:30:40 +0200

    Revert "Final II update before new blockchain release"
    This reverts commit 6d5eb398a9be33b2a06ece874706db259a313cde.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return 9337;
+    return 9338;
 }
 
 Object JSONRPCError(int code, const string& message)
