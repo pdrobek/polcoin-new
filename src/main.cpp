a05331cd50fe6a484d295469fc5e4d9757cc191e
Rav3nPL <rav3n.pl@gmail.com>
2017-05-31 16:25:53 +0200

    Update main.cpp
    remove unused KGW code

--- src/main.cpp
+++ src/main.cpp
@@ -1141,58 +1141,6 @@ unsigned int static GetNextWorkRequired_V1(const CBlockIndex* pindexLast, const 
     return bnNew.GetCompact();
 }
 
-unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBlockHeader *pblock, uint64 TargetBlocksSpacingSeconds, uint64 PastBlocksMin, uint64 PastBlocksMax) {
-/* current difficulty formula, megacoin - kimoto gravity well */
-const CBlockIndex *BlockLastSolved = pindexLast;
-const CBlockIndex *BlockReading = pindexLast;
-const CBlockHeader *BlockCreating = pblock;
-BlockCreating = BlockCreating;
-uint64 PastBlocksMass = 0;
-int64 PastRateActualSeconds = 0;
-int64 PastRateTargetSeconds = 0;
-double	PastRateAdjustmentRatio = double(1);
-CBigNum PastDifficultyAverage;
-CBigNum PastDifficultyAveragePrev;
-double	EventHorizonDeviation;
-double	EventHorizonDeviationFast;
-double	EventHorizonDeviationSlow;
-if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit.GetCompact(); }
-for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
-if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
-PastBlocksMass++;
-if (i == 1) { PastDifficultyAverage.SetCompact(BlockReading->nBits); }
-else	{ PastDifficultyAverage = ((CBigNum().SetCompact(BlockReading->nBits) - PastDifficultyAveragePrev) / i) + PastDifficultyAveragePrev; }
-PastDifficultyAveragePrev = PastDifficultyAverage;
-PastRateActualSeconds = BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
-PastRateTargetSeconds = TargetBlocksSpacingSeconds * PastBlocksMass;
-PastRateAdjustmentRatio = double(1);
-if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
-if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
-PastRateAdjustmentRatio = double(PastRateTargetSeconds) / double(PastRateActualSeconds);
-}
-EventHorizonDeviation = 1 + (0.7084 * pow((double(PastBlocksMass)/double(144)), -1.228));
-EventHorizonDeviationFast = EventHorizonDeviation;
-EventHorizonDeviationSlow = 1 / EventHorizonDeviation;
-if (PastBlocksMass >= PastBlocksMin) {
-if ((PastRateAdjustmentRatio <= EventHorizonDeviationSlow) || (PastRateAdjustmentRatio >= EventHorizonDeviationFast)) { assert(BlockReading); break; }
-}
-if (BlockReading->pprev == NULL) { assert(BlockReading); break; }
-BlockReading = BlockReading->pprev;
-}
-CBigNum bnNew(PastDifficultyAverage);
-if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
-bnNew *= PastRateActualSeconds;
-bnNew /= PastRateTargetSeconds;
-}
-if (bnNew > bnProofOfWorkLimit) { bnNew = bnProofOfWorkLimit; }
-/// debug print
-printf("Difficulty Retarget - Kimoto Gravity Well\n");
-printf("PastRateAdjustmentRatio = %g\n", PastRateAdjustmentRatio);
-printf("Before: %08x %s\n", BlockLastSolved->nBits, CBigNum().SetCompact(BlockLastSolved->nBits).getuint256().ToString().c_str());
-printf("After: %08x %s\n", bnNew.GetCompact(), bnNew.getuint256().ToString().c_str());
-return bnNew.GetCompact();
-}
-
 unsigned int static DarkGravityWave3(const CBlockIndex* pindexLast, const CBlockHeader *pblock) {
     /* current difficulty formula, darkcoin - DarkGravity v3, written by Evan Duffield - evan@darkcoin.io */
     const CBlockIndex *BlockLastSolved = pindexLast;
@@ -1269,17 +1217,6 @@ unsigned int static DarkGravityWave3(const CBlockIndex* pindexLast, const CBlock
 
     // Return the new diff.
     return bnNew.GetCompact();
-}
-
-unsigned int static GetNextWorkRequired_V2(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
-{
-static const int64 BlocksTargetSpacing = 60;
-unsigned int	TimeDaySeconds = 60 * 60 * 24;
-int64 PastSecondsMin = TimeDaySeconds * 0.25;
-int64 PastSecondsMax = TimeDaySeconds * 7;
-uint64 PastBlocksMin = PastSecondsMin / BlocksTargetSpacing;
-uint64 PastBlocksMax = PastSecondsMax / BlocksTargetSpacing;
-return KimotoGravityWell(pindexLast, pblock, BlocksTargetSpacing, PastBlocksMin, PastBlocksMax);
 }
 
 unsigned int static GetNextWorkRequired_V3(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
