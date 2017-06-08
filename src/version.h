45af7c918cdf7f281dd032dd431a78223406a912
Paweł Drobek <pawel.drobek@gmail.com>
2017-06-08 10:58:11 +0200

    revert last changes in version.h due to issues releated with polcoinj

--- src/version.h
+++ src/version.h
@@ -25,10 +25,10 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 70001;
 
 // earlier versions not supported 
-static const int MIN_PROTO_VERSION = 70001;
+static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
